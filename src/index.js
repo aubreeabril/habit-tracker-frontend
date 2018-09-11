4031ace8148f9d2aa3a4283646370f55799a65d8
john-raber <jrab224@gmail.com>
2018-09-11 11:21:48 -0400

    added habit form, patch still does not work

--- src/index.js
+++ src/index.js
@@ -1,46 +1,74 @@
-const USERS_URL = 'http://localhost:3000/users'
-const HABITS_URL = 'http://localhost:3000/habits'
+const USERS_URL = "http://localhost:3000/users";
+const HABITS_URL = "http://localhost:3000/habits";
+const USER_HABITS_URL = "http://localhost:3000/user_habits";
 
-document.addEventListener('DOMContentLoaded', init)
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  fetchUsers()
+  fetchUsers();
 }
 
 function fetchUsers() {
   fetch(`${USERS_URL}`)
-  .then(response => response.json())
-  .then(json => {
-    json.forEach(user => {
-      // debugger
-      let newUser = new User(user)
-      newUser.render()
-    })
-  })
+    .then(response => response.json())
+    .then(json => {
+      json.forEach(user => {
+        // debugger
+        let newUser = new User(user);
+        newUser.render();
+      });
+    });
 }
 
 function checkBox() {
-let userHabit
-let userId = parseInt(event.target.dataset.userId)
-let habitId = parseInt(event.target.dataset.habitId)
+  let userHabit;
+  let userId = parseInt(event.target.dataset.userId);
+  let habitId = parseInt(event.target.dataset.habitId);
+  let checked = event.target.checked;
+  let fullDate = event.target.dataset.fullDate;
   fetch(`http://localhost:3000/user_habits`)
-  .then(response => response.json())
-  .then(json =>  json.forEach(uh => {
-    if (uh.user_id == userId && uh.habit_id == habitId) {
-      userHabit = uh
-    }
-  })
+    .then(response => response.json())
+    .then(json =>
+      json.forEach(uh => {
+        // debugger;
+        if (uh.user_id == userId && uh.habit_id == habitId && checked) {
+          // debugger;
+          // userHabit = uh;
+          patchUserHabit(fullDate, uh);
+          // console.log(userHabit);
+        }
+      })
+    );
 
+  // console.log(userHabit);
 
-  )
-
-  console.log(userHabit)
-
-  if (event.target.checked) {
-    patchUserHabit(event.target.dataset.fullDate)
-  }
+  // if (event.target.checked) {
+  //   patchUserHabit(event.target.dataset.fullDate);
+  // }
 }
 
-function patchUserHabit(date) {
+function patchUserHabit(date, userHabit) {
+  // console.log(date);
+  // console.log(userHabit);
+  // console.log(userHabit.dates);
 
+  let data = userHabit;
+  let newDates = userHabit.dates;
+  newDates.push(date);
+  data.dates = newDates;
+
+  console.log(data);
+
+  fetch(`${USER_HABITS_URL}/${userHabit.id}`, {
+    method: "PATCH",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      dates: "1234"
+    })
+  })
+    .then(r => r.json())
+    .then(json => console.log(json));
 }
--- src/user.js
+++ src/user.js
@@ -1,54 +1,75 @@
 class User {
   constructor(args) {
-    this.name = args.name
-    this.age = args.age
-    this.gender = args.gender
-    this.id = args.id
-    this.habits = args.habits
+    this.name = args.name;
+    this.age = args.age;
+    this.gender = args.gender;
+    this.id = args.id;
+    this.habits = args.habits;
   }
 
   render() {
-    let list = document.getElementById('users-list')
-    let newItem = document.createElement('li')
-    newItem.innerText = this.name
-    newItem.addEventListener('click', e => {
-      this.show()
-    })
+    let list = document.getElementById("users-list");
+    let newItem = document.createElement("li");
+    newItem.innerText = this.name;
+    newItem.addEventListener("click", e => {
+      this.show();
+    });
 
-    list.appendChild(newItem)
+    list.appendChild(newItem);
   }
 
   show() {
-    document.querySelector('#users-list').innerHTML = ''
+    document.querySelector("#users-list").innerHTML = "";
 
-    let userInfo = document.createElement('ul')
-    userInfo.id = `info-${this.id}`
-    userInfo.className = 'h-list'
+    let userInfo = document.createElement("ul");
+    userInfo.id = `info-${this.id}`;
+    userInfo.className = "h-list";
 
-    let nameLi = document.createElement('li')
-    let ageLi = document.createElement('li')
-    let genderLi = document.createElement('li')
+    let nameLi = document.createElement("li");
+    let ageLi = document.createElement("li");
+    let genderLi = document.createElement("li");
 
     // debugger
-    nameLi.innerText = this.name
-    ageLi.innerText = this.age
-    genderLi.innerText = this.gender
+    nameLi.innerText = this.name;
+    ageLi.innerText = this.age;
+    genderLi.innerText = this.gender;
 
-    document.querySelector('#main').appendChild(userInfo)
-    userInfo.appendChild(nameLi)
-    userInfo.appendChild(ageLi)
-    userInfo.appendChild(genderLi)
+    document.querySelector("#main").appendChild(userInfo);
+    userInfo.appendChild(nameLi);
+    userInfo.appendChild(ageLi);
+    userInfo.appendChild(genderLi);
 
-    let habitList = document.createElement('table')
-    habitList.id = `habit-table`
-    let habitDiv = document.createElement('div')
+    let habitList = document.createElement("table");
+    habitList.id = `habit-table`;
+    let habitDiv = document.createElement("div");
 
-    userInfo.appendChild(habitDiv)
-    habitDiv.appendChild(habitList)
+    document.querySelector("#main").appendChild(habitDiv);
+    habitDiv.appendChild(habitList);
 
     this.habits.forEach(habit => {
-      let newHabit = new Habit(habit)
-      newHabit.render(this)
-    })
+      let newHabit = new Habit(habit);
+      newHabit.render(this);
+    });
+
+    // Creating form for habit
+    let h2Element = document.createElement("h2");
+    let habitForm = document.createElement("form");
+    let titleInput = document.createElement("input");
+    let descriptionInput = document.createElement("input");
+    let habitFormSubmit = document.createElement("input");
+    habitFormSubmit.type = "submit";
+    habitFormSubmit.dataset.userId = this.id;
+    habitFormSubmit.addEventListener("click", e => {
+      console.log(e);
+    });
+    h2Element.innerText = "Add New Habit";
+    titleInput.placeholder = "title";
+    descriptionInput.placeholder = "description";
+
+    document.querySelector("#main").appendChild(habitForm);
+    habitForm.appendChild(h2Element);
+    habitForm.appendChild(titleInput);
+    habitForm.appendChild(descriptionInput);
+    habitForm.appendChild(habitFormSubmit);
   }
 }
